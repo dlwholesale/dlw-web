87046fc4f6c6d116aa6ab9951688ee5cac1e5885
dlwholesale <donald@dlwholesale.com>
2025-04-06 18:09:36 -0400

    Production environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://ajqkkiyzdsylzgkaoymq.supabase.co'
+  supabaseUrl: 'https://ajqkkiyzdsylzgkaoymq.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqcWtraXl6ZHN5bHpna2FveW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODgzMDEsImV4cCI6MjA1OTM2NDMwMX0.ToewLC7o9mEME7w-DYLm0vurZvzZaCLEjZEzjrMEPkM',
   apiUrl: 'https://dlw-api-9iod.onrender.com'
 };
