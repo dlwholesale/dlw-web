956d984cbceed35213b44c822c6d6d6bbe5bc47b
dlwholesale <donald@dlwholesale.com>
2025-04-06 18:08:17 -0400

    Production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://yqonongwueemcwnpihnn.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlxb25vbmd3dWVlbWN3bnBpaG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMzQ3NDIsImV4cCI6MjA1NjYxMDc0Mn0.8oiAOna72yf71DP471RS8QXSRXId5eL2meLsxqZJZxw',
-  apiUrl: 'https://dlw-api.onrender.com'
+  supabaseUrl: 'https://ajqkkiyzdsylzgkaoymq.supabase.co'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqcWtraXl6ZHN5bHpna2FveW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODgzMDEsImV4cCI6MjA1OTM2NDMwMX0.ToewLC7o9mEME7w-DYLm0vurZvzZaCLEjZEzjrMEPkM',
+  apiUrl: 'https://dlw-api-9iod.onrender.com'
 };
