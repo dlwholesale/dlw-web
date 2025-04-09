63ef1adeb53825e188e29f092199a99b433af25e
dlwholesale <donald@dlwholesale.com>
2025-04-09 00:10:07 -0400

    Bug fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://yqonongwueemcwnpihnn.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlxb25vbmd3dWVlbWN3bnBpaG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMzQ3NDIsImV4cCI6MjA1NjYxMDc0Mn0.8oiAOna72yf71DP471RS8QXSRXId5eL2meLsxqZJZxw',
-  apiUrl: 'https://dlw-api.onrender.com'
+  apiUrl: 'https://dlw-api-9iod.onrender.com'
 };
