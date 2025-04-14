4464f3ffcab6172ca7d2f8a4c262b85806861d41
Reynier <reynierp.salas@gmail.com>
2025-04-14 00:21:05 -0400

    Updates

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   // supabaseUrl: 'https://yqonongwueemcwnpihnn.supabase.co', // Staging
   // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlxb25vbmd3dWVlbWN3bnBpaG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwMzQ3NDIsImV4cCI6MjA1NjYxMDc0Mn0.8oiAOna72yf71DP471RS8QXSRXId5eL2meLsxqZJZxw',
-  supabaseUrl: 'https://ajqkkiyzdsylzgkaoymq.supabase.co', // Production
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqcWtraXl6ZHN5bHpna2FveW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM3ODgzMDEsImV4cCI6MjA1OTM2NDMwMX0.ToewLC7o9mEME7w-DYLm0vurZvzZaCLEjZEzjrMEPkM',
+  supabaseUrl: 'https://yckmkpqrrfijdfvwmrpf.supabase.co', // Production
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlja21rcHFycmZpamRmdndtcnBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2MDI1MTksImV4cCI6MjA2MDE3ODUxOX0.zhEOHuJVQedoWh3LP2zdSjtF3RXoT9SdKwa9fBpe7To',
   // apiUrl: 'https://dlw-api.onrender.com' // Staging
   // apiUrl: 'http://localhost:3000'
   apiUrl: 'https://dlw-api-9iod.onrender.com' // Production
